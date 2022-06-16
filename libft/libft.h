5da841a254ce96d779cd236d7463805efcde8959
Deniz Surmeli <denizsurmeli@icloud.com>
2022-06-16 18:32:19 +0300

    implement ft_is* and ft_to*

--- libft/libft.h
+++ libft/libft.h
@@ -34,5 +34,11 @@ char *ft_strstr(const char *haystack, const char *needle);
 int ft_strcmp(const char *s1, const char *s2);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 int ft_atoi(const char *src);
-
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+int ft_toupper(int c);
+int ft_tolower(int c);
 #endif
